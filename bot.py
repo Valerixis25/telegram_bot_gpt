3c379d888e5110dfdcd8d6bdf116e7883b4af209
Valerixis25 <gubrisha16@gmail.com>
2026-07-16 00:07:47 +0300

    feat: gpt feature added,gpt_buttons_handler added, plain_text_handler added, gpt chat mode created

--- bot.py
+++ bot.py
@@ -1,25 +1,16 @@
 from http.client import responses
 
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CallbackQueryHandler, ContextTypes, CommandHandler
+from telegram.ext import ApplicationBuilder, CallbackQueryHandler, ContextTypes, CommandHandler, MessageHandler
 
 from gpt import ChatGptService
 from util import (load_message, send_text, send_image, show_main_menu,
                   default_callback_handler, load_prompt, send_text_buttons)
 
+
 import credentials
 
-
-# 1. *"Випадковий факт"*
-# Телеграм-бот повинен обробляти команду /random.
-# При обробці команди він надсилає заздалегідь підготовлене зображення
-# та робить запит до ChatGPT із заздалегідь підготовленим промптом.
-# Відповідь ChatGPT потрібно отримати та передати користувачеві.
-# До повідомлення має бути прикріплена кнопка "Закінчити", натискання на яку
-# працює так само, як команда /start.
-# І кнопка "Хочу ще факт", натискання на яку
-# працює так само, як команда /random
-
+chat_modes = {}
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = load_message('main')
@@ -40,9 +31,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def random(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await send_image(update, context, 'random')
     prompt = load_prompt('random')
-    # response = await chat_gpt.send_question(prompt, 'Давай рандомний факт')
-    response = 'Interesting fact'
-    # await send_text(update, context, response)
+    response = await chat_gpt.send_question(prompt, 'Давай рандомний факт')
     await send_text_buttons(
         update, context,
         response,
@@ -52,6 +41,26 @@ async def random(update: Update, context: ContextTypes.DEFAULT_TYPE):
         }
     )
 
+async def gpt(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat_modes[update.message.from_user.id] = 'GPT_MODE'
+    await send_image(update, context, 'gpt')
+    await send_text(update, context, load_message('gpt'))
+
+async def plain_text_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    mode = chat_modes.get(update.message.from_user.id)
+    if mode is None:
+        await send_text(update, context, 'Я не розумію такої команди. Відправ команду /start для допомоги')
+    elif mode == 'GPT_MODE':
+        prompt = load_prompt('gpt')
+        response = await chat_gpt.send_question(prompt, update.message.text)
+        await send_text_buttons(update, context, response,
+                            {
+                                'gpt_finish': 'Закінчити',
+                                'gpt_one_more' : 'Маю ще питання'
+                            }
+                    )
+
+
 async def random_buttons_handler(update: Update, context):
     query = update.callback_query.data
     if query == 'random_finish':
@@ -60,14 +69,24 @@ async def random_buttons_handler(update: Update, context):
         await random(update, context)
     await update.callback_query.answer()
 
+async def gpt_buttons_handler(update: Update, context):
+    query = update.callback_query.data
+    if query == 'gpt_finish':
+        await start(update, context)
+        chat_modes[update.message.from_user.id] = None
+
 chat_gpt = ChatGptService(credentials.ChatGPT_TOKEN)
 app = ApplicationBuilder().token(credentials.BOT_TOKEN).build()
 
 # Зареєструвати обробник команди можна так:
 app.add_handler(CommandHandler('start', start))
 app.add_handler(CommandHandler('random', random))
+app.add_handler(CommandHandler('gpt', gpt))
+app.add_handler(MessageHandler(None, plain_text_handler))
 
 # Зареєструвати обробник колбеку можна так:
 app.add_handler(CallbackQueryHandler(random_buttons_handler, pattern='^random_.*'))
+app.add_handler(CallbackQueryHandler(gpt_buttons_handler, pattern='^gpt_.*'))
+
 # app.add_handler(CallbackQueryHandler(default_callback_handler))
 app.run_polling()
